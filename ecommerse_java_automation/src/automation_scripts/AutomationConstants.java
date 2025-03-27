73a2cb6648a493e4e73965c699628d8e16781397
igurupreeth <igurupreeth@gmail.com>
2025-03-27 14:34:15 +0530

    updated urls in automation constants

--- ecommerse_java_automation/src/automation_scripts/AutomationConstants.java
+++ ecommerse_java_automation/src/automation_scripts/AutomationConstants.java
@@ -5,6 +5,10 @@ public interface AutomationConstants
     String urlOfHomepage = "http://localhost:5173";
     
     String urlOfLogin = "http://localhost:5173/login";
+    String urlOfRegister = "http://localhost:5173/register";
+    String urlOfContact = "http://localhost:5173/contact-us";
+    String urlOfAbout = "http://localhost:5173/about-us";
+    String urlOfDashboard = "http://localhost:5173/dashboard";
     
     String rootFolder = System.getProperty("user.dir");
     
