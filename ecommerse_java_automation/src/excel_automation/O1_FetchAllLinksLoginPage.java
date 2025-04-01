76b0edfd383fec204a889187330eae1d77743221
igurupreeth <igurupreeth@gmail.com>
2025-04-01 11:43:15 +0530

    updated fetch links from login page

--- ecommerse_java_automation/src/excel_automation/O1_FetchAllLinksLoginPage.java
+++ ecommerse_java_automation/src/excel_automation/O1_FetchAllLinksLoginPage.java
@@ -1,5 +1,6 @@
 package excel_automation;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -11,30 +12,48 @@ import automation_scripts.AutomationConstants;
 
 public class O1_FetchAllLinksLoginPage implements AutomationConstants{
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
-		System.out.println("Fetch all links from Login page");
-		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get(urlOfLogin);
-		
-		System.out.println(driver.getTitle());
-		
-		System.out.println(driver.getCurrentUrl());
-		
-		List <WebElement> allLinks = driver.findElements(By.tagName("a"));
-		
-		System.out.println("Total links in login page " + allLinks.size());
-		
-		for ( WebElement eachLink  :allLinks)
-		{ 
-			String text = eachLink.getText();
+		WebDriver driver = null; 
+		try
+		{
+			System.out.println("Fetch all links from Login page");
 			
-			 String linkAddress = eachLink.getAttribute("href");
-			 
-			 System.out.println(text +" " + linkAddress);
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.manage().window().maximize();
 			
+			driver.get(urlOfLogin);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			
+			System.out.println(driver.getTitle());
+			
+			System.out.println(driver.getCurrentUrl());
+			
+			List <WebElement> allLinks = driver.findElements(By.tagName("a"));
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			
+			System.out.println("Total links in login page " + allLinks.size());
+			
+			for ( WebElement eachLink  :allLinks)
+			{ 
+				String text = eachLink.getText();
+				
+				 String linkAddress = eachLink.getAttribute("href");
+				 
+				 System.out.println(text +" " + linkAddress);
+				
+			}
 		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			Thread.sleep(3000);
+			driver.quit();
+		}
+
 	}
 }
