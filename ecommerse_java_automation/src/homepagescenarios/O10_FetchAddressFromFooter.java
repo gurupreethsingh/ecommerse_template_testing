a97968dfe16be8da356349306d5a787fbd92dced
NShilpamurthhy <nshilpamurthy@gmail.com>
2025-04-03 14:30:39 +0530

    fetch address updated

--- ecommerse_java_automation/src/homepagescenarios/O10_FetchAddressFromFooter.java
+++ ecommerse_java_automation/src/homepagescenarios/O10_FetchAddressFromFooter.java
@@ -25,48 +25,53 @@ public class O10_FetchAddressFromFooter implements AutomationConstants {
 			driver.get(urlOfHomepage);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.manage().window().maximize();
-			
-			
-//			((JavascriptExecutor) driver).executeScript("window.scrollBy(0, document.body.scrollHeight)");
-//			Thread.sleep(1000);
-			
-			// find address.
-			 WebElement addressText = driver.findElement(By.xpath("(//p)[2]"));
-			 
-			 JavascriptExecutor js = (JavascriptExecutor) driver;
-	          Thread.sleep(1000);
-	          js.executeScript("arguments[0].scrollIntoView(true)",    addressText );
-	          Thread.sleep(1000);
-	          //find the shop link. 
 
-	          int paranumber=1;
-	          for(int i = 2; i<= 3; i++)
-	          {
-	        	 
-	        	 String AddressectionTexts =  driver.findElement(By.xpath("(//p)["+i+"]")).getText();
-	        	 System.out.println(paranumber +" :: " + AddressectionTexts);
-	        	 paranumber++;
-	        	 System.out.println("\n*******************************************************\n");
-	        	 Thread.sleep(500);
-	          }
-		
-			
-		}
-			catch (Exception ex) 
-			{
+			// printing Address heading
+			WebElement addText = driver.findElement(By.xpath("//div[@class='min-w-[200px]']/h2"));
+			System.out.println(addText.getText());
+
+			// ecoders address
+			WebElement addressText = driver.findElement(By.xpath("(//p)[2]"));
+			// System.out.println(addressText.getText());
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			Thread.sleep(1000);
+			js.executeScript("arguments[0].scrollIntoView(true)", addressText);
+			Thread.sleep(1000);
+
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+			String expectedText = "ECODERS\r\n" + "#198, Defence Colony,\r\n" + "Hesaraghatta Road,\r\n"
+					+ "Bangalore 560073.";
+
+			System.out.println(expectedText);
+			System.out.println("-----------------------------------");
+
+			try {
+				WebElement actualAddressText = wait.until(ExpectedConditions
+						.visibilityOfElementLocated(By.xpath("//p[@class='text-sm font-medium leading-relaxed']")));
+				System.out.println(actualAddressText.getText());
+				System.out.println("-----------------------------------");
+
+				if (expectedText.equals(actualAddressText.getText())) {
+					System.out.println("text is matched");
+				} else {
+					System.out.println("text is not matched");
+				}
+
+			} catch (Exception ex) {
 				// Clean handling — no stack trace
-				System.out.println("Test failed: Expected title did not match. Screenshot captured.");
+				System.out.println("Test failed: Expected textdid not match. Screenshot captured.");
 				TakeScreenshot.getScreenshot(driver);
 				System.out.println("Testcase failed screenshot stored.");
-			} 
-			finally 
-			{
-				driver.quit();
 			}
-		
+		} catch (Exception ex) {
+			// Clean handling — no stack trace
+			System.out.println("Test failed: Expected title did not match. Screenshot captured.");
+			TakeScreenshot.getScreenshot(driver);
+			System.out.println("Testcase failed screenshot stored.");
+		} finally {
+			driver.quit();
+		}
+
 	}
 }
-
-	
-
-	
