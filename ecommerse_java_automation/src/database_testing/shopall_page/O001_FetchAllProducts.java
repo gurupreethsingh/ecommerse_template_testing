3a79b93bd8cdba6ebc11ced0f4742f28bf3decd2
Gurupreeth <igurupreet@gmail.com>
2025-06-03 05:32:52 +0530

    updated more scenarios in shop all page along with regression scenarios

---
+++ ecommerse_java_automation/src/database_testing/shopall_page/O001_FetchAllProducts.java
@@ -0,0 +1,9 @@
+package database_testing.shopall_page;
+
+public class O001_FetchAllProducts {
+
+	public static void main(String[] args)
+	{
+      // fetching all the products from database. 
+	}
+}
